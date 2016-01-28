b260bd598ed434dbbb96d0ea96e617b52d2fd18c
Scsilver <scsilver.umd@gmail.com>
2016-01-27 23:58:48 -0700

    Add inititation factor test

--- lib/agnet.rb
+++ lib/agnet.rb
@@ -1,12 +1,17 @@
-require "agnet/version"
+require 'agnet/version'
 
 class Agnet
   def new
     @agnet = 1
   end
-  def set_initial_weights
-  end
+
   def feed_forward
+    
   end
+
+  def set_initial_weights
+
+  end
+
   # Your code goes here...
 end
--- spec/agnet_spec.rb
+++ spec/agnet_spec.rb
@@ -1,62 +1,75 @@
 require 'spec_helper'
 
-
 describe Agnet do
   it 'has a version number' do
     expect(Agnet::VERSION).not_to be nil
   end
+  describe 'private instance methods' do
+    subject { Agnet.new(4, 16) }
 
-  describe "public instance methods" do
-    subject { Agnet.new }
-
-
-    let(:input) { [input_activation,weights_hidden,weights_output,input_nodes,hidden_nodes,output_nodes] }
-    let(:output) { subject.process(input) }
-    context "responds to its methods" do
-
-      it { expect(subject).to respond_to(:feed_forward) }
+    context 'responds to its methods' do
       it { expect(subject).to respond_to(:set_initial_weights) }
-
     end
 
-    context "executes methods correctly" do
-      context "#feed_forward" do
-        it "calcs weighted sum for each node in hidden layer with hidden weights" do
-
-	       end
-
-        it "activates each node in hidden layer" do
-
-	       end
-
-	      it "cals weighted sum for each node in output layer with output weights" do
-
-	      end
-
-	      it "activates each node in output layer" do
-
-	      end
-
-      end
-      context "#set_initial_weights" do
-        it "calcs initial factor for hidden weight matrix" do
+    context 'executes methods correctly' do
+      context '#set_initial_factorss' do
+        it 'has array of initial factors for hidden and output weight matrix' do
+          expects(subject.set_initial_factors.size).to eq(2)
         end
-        it "calcs initial factor for output weight matrix" do
-        end
-        it "sets initial hidden layer weight matrix" do
-        end
-        it "sets initial output layer weight matrix" do
-        end
-        it "sets initial hidden weight matrix values between 0.5 and -0.5" do
-        end
-        it "sets initial output weight matrix values between 0.5 and -0.5" do
-        end
-        it "mulitplys hidden initial factor to hidden weight matrix cales" do
-        end
-        it "mulitplys output initial factor to hidden weight matrix cales" do
+        it 'has array values according to function  1 / inputsize^0.5' do
+          expects(subject.set_initial_factors[0]).to eq(0.5)
+          expects(subject.set_initial_factors[1]).to eq(0.25)
         end
       end
+      context '#set_initial_weights' do
+        it 'calcs initial factor for hidden weight matrix' do
+        end
+        it 'calcs initial factor for output weight matrix' do
+        end
+        it 'sets initial hidden layer weight matrix' do
+        end
+        it 'sets initial output layer weight matrix' do
+        end
+        it 'sets initial hidden weight matrix values between 0.5 and -0.5' do
+        end
+        it 'sets initial output weight matrix values between 0.5 and -0.5' do
+        end
+        it 'mulitplys hidden initial factor to hidden weight matrix cales' do
+        end
+        it 'mulitplys output initial factor to hidden weight matrix cales' do
+        end
+      end
+    end
+  end
 
+  describe 'public instance methods' do
+    subject { Agnet.new }
+
+    let(:input) do
+      [input_activation, weights_hidden, weights_output,
+       input_nodes, hidden_nodes, output_nodes]
+    end
+
+    let(:output) { subject.process(input) }
+    context 'responds to its methods' do
+      it { expect(subject).to respond_to(:feed_forward) }
+      it { expect(subject).to respond_to(:set_initial_weights) }
+    end
+
+    context 'executes methods correctly' do
+      context '#feed_forward' do
+        it 'calcs weighted sum for each node in hdn layer with hdn weights' do
+        end
+
+        it 'activates each node in hidden layer' do
+        end
+
+        it 'cals weighted sum for each node in outpt layer with otpt weights' do
+        end
+
+        it 'activates each node in output layer' do
+        end
+      end
     end
   end
 end
