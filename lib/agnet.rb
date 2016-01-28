f9b62c176831d950b064502017fd11ea509bed4c
Scsilver <scsilver.umd@gmail.com>
2016-01-28 16:40:51 -0700

    Pass scale_initial_weights

--- lib/agnet.rb
+++ lib/agnet.rb
@@ -1,4 +1,7 @@
 require 'agnet/version'
+require 'csv'
+require 'matrix'
+require 'pry'
 
 class Agnet
   def initialize(input_nodes, hidden_nodes, output_nodes)
@@ -10,12 +13,37 @@ class Agnet
   def feed_forward
 
   end
-
+  def scale_initial_weights()
+    set_initial_factors.map.with_index{ | a, i | a * @initial_weights[i] }
+  end
   def set_initial_factors
     hdn_init_factor = 1 / ((@in_nodes)**(0.5))
     [hdn_init_factor] << 1 / (@hdn_nodes**(0.5))
   end
-
   def set_initial_weights
+    h_w = []
+    o_w = []
+    @hdn_nodes.times do |i|
+      if i == 0
+        h_w = (Vector.elements(Array.new(@in_nodes+1) { (rand(0.1) - 0.5) }))
+              .covector
+      else
+        n_w = (Vector.elements(Array.new(@in_nodes+1) { (rand(0.1) - 0.5) }))
+              .covector
+        h_w = h_w.vstack(n_w)
+      end
+    end
+    @out_nodes.times do |i|
+        if i == 0
+          o_w = (Vector.elements(Array.new(@hdn_nodes+1) { (rand(0.1) - 0.5) }))
+                .covector
+        else
+          l_w = (Vector.elements(Array.new(@hdn_nodes+1) { (rand(0.1) - 0.5) }))
+                .covector
+          o_w = o_w.vstack(l_w)
+        end
+      end
+    @initial_weights = [h_w]
+    @initial_weights << o_w
   end
 end
--- spec/agnet_spec.rb
+++ spec/agnet_spec.rb
@@ -6,6 +6,7 @@ describe Agnet do
   end
 
   describe 'public instance methods' do
+
     subject { Agnet.new(4, 16, 10) }
 
     let(:input) do
@@ -18,35 +19,44 @@ describe Agnet do
       it { expect(subject).to respond_to(:feed_forward) }
       it { expect(subject).to respond_to(:set_initial_weights) }
       it { expect(subject).to respond_to(:set_initial_factors) }
-
     end
 
     context 'executes methods correctly' do
       context '#set_initial_factorss' do
-        it 'has array of initial factors for hidden and output weight matrix' do
+        it 'sets array of initial factors for hdn and output weight matrix' do
           expect(subject.set_initial_factors.size).to eq(2)
         end
-        it 'has array values according to function  1 / inputsize^0.5' do
+        it 'sets array values according to function  1 / inputsize^0.5' do
           expect(subject.set_initial_factors[0]).to eq(0.5)
           expect(subject.set_initial_factors[1]).to eq(0.25)
         end
       end
       context '#set_initial_weights' do
-        it 'calcs initial factor for hidden weight matrix' do
-        end
-        it 'calcs initial factor for output weight matrix' do
-        end
         it 'sets initial hidden layer weight matrix' do
+          expect(subject.set_initial_weights[0].column_count).to eq(5)
+          expect(subject.set_initial_weights[0].row_count).to eq(16)
         end
         it 'sets initial output layer weight matrix' do
+          expect(subject.set_initial_weights[1].column_count).to eq(17)
+          expect(subject.set_initial_weights[1].row_count).to eq(10)
         end
         it 'sets initial hidden weight matrix values between 0.5 and -0.5' do
+          expect(subject.set_initial_weights[0][0,0]).to be_within(0.5).of(0)
         end
         it 'sets initial output weight matrix values between 0.5 and -0.5' do
+          expect(subject.set_initial_weights[1][0,0]).to be_within(0.5).of(0)
         end
-        it 'mulitplys hidden initial factor to hidden weight matrix cales' do
+      end
+      context '#scale_initial_weights' do
+        it 'mulitplys hidden initial factor to hidden weight matrix calcs' do
+          init_weights = subject.set_initial_weights
+          init_factors = subject.set_initial_factors
+          expect(subject.scale_initial_weights[0][0,0]).to eq(init_weights[0][0,0]*init_factors[0])
         end
-        it 'mulitplys output initial factor to hidden weight matrix cales' do
+        it 'mulitplys output initial factor to ouput weight matrix calcs' do
+          init_weights = subject.set_initial_weights
+          init_factors = subject.set_initial_factors
+          expect(subject.scale_initial_weights[1][0,0]).to eq(init_weights[1][0,0]*init_factors[1])
         end
       end
       context '#feed_forward' do
