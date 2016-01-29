7cfa1f74f589f03ca1aafa5ba549602d6e47e5c9
Scsilver <scsilver.umd@gmail.com>
2016-01-28 23:48:12 -0700

    Pass all tests for forward step of neural net

--- lib/agnet.rb
+++ lib/agnet.rb
@@ -4,46 +4,114 @@ require 'matrix'
 require 'pry'
 
 class Agnet
-  def initialize(input_nodes, hidden_nodes, output_nodes)
+  def initialize(input_nodes, hidden_nodes, output_nodes, function,
+                 input_activation, input_bias, hidden_bias, bits)
     @in_nodes = input_nodes
     @hdn_nodes = hidden_nodes
     @out_nodes = output_nodes
+    @function = function
+    @input_activation = input_activation
+    @input_bias = input_bias
+    @hidden_bias = hidden_bias
+    @bits = bits
   end
 
   def feed_forward
+  end
 
+  def scale_initial_weights
+    set_initial_factors.map.with_index { |a, i| a * @initial_weights[i] }
   end
-  def scale_initial_weights()
-    set_initial_factors.map.with_index{ | a, i | a * @initial_weights[i] }
-  end
+
   def set_initial_factors
-    hdn_init_factor = 1 / ((@in_nodes)**(0.5))
-    [hdn_init_factor] << 1 / (@hdn_nodes**(0.5))
+    hdn_init_factor = 1 / (@in_nodes**0.5)
+    [hdn_init_factor] << 1 / (@hdn_nodes**0.5)
   end
+
   def set_initial_weights
     h_w = []
     o_w = []
     @hdn_nodes.times do |i|
       if i == 0
-        h_w = (Vector.elements(Array.new(@in_nodes+1) { (rand(0.1) - 0.5) }))
-              .covector
+        h_w = Vector.elements(Array.new(@in_nodes + 1) { (rand(0.1) - 0.5) })
+                    .covector
       else
-        n_w = (Vector.elements(Array.new(@in_nodes+1) { (rand(0.1) - 0.5) }))
-              .covector
+        n_w = Vector.elements(Array.new(@in_nodes + 1) { (rand(0.1) - 0.5) })
+                    .covector
         h_w = h_w.vstack(n_w)
       end
     end
     @out_nodes.times do |i|
-        if i == 0
-          o_w = (Vector.elements(Array.new(@hdn_nodes+1) { (rand(0.1) - 0.5) }))
-                .covector
-        else
-          l_w = (Vector.elements(Array.new(@hdn_nodes+1) { (rand(0.1) - 0.5) }))
-                .covector
-          o_w = o_w.vstack(l_w)
-        end
+      if i == 0
+        o_w = Vector.elements(Array.new(@hdn_nodes + 1) { (rand(0.1) - 0.5) })
+                    .covector
+      else
+        l_w = Vector.elements(Array.new(@hdn_nodes + 1) { (rand(0.1) - 0.5) })
+                    .covector
+        o_w = o_w.vstack(l_w)
       end
+    end
     @initial_weights = [h_w]
     @initial_weights << o_w
   end
+
+  def normalize_input_activation
+    array = Array.new(@in_nodes + 1)
+    array = @input_activation
+    array[@in_nodes] = @input_bias
+    Vector.elements(array) / @bits
+  end
+
+  def hidden_layer_weighted_sum
+    z_v_h = []
+    @hdn_nodes.times do |i|
+      z_v_h << scale_initial_weights[0].row(i).inner_product(normalize_input_activation)
+    end
+    return z_v_h
+  end
+
+  def hidden_layer_activation
+    activation_function(hidden_layer_weighted_sum)
+  end
+
+  def activation_function(z)
+    fin = Array.new(z.size)
+    if @function == 'sigmax'
+
+      res = Array.new(z.size)
+      z.each_with_index do |val, i|
+        res[i] = Math.exp(val)
+      end
+
+
+      sum = res.inject(:+)
+      res.each_with_index do |val, i|
+
+
+        fin[i] = val / sum
+      end
+    end
+
+    return fin
+  end
+
+  def vectorize_hidden_layer
+    array = Array.new(@hdn_nodes + 1)
+    array = hidden_layer_activation
+    array[@hdn_nodes] = @hidden_bias
+    Vector.elements(array)
+  end
+
+  def output_layer_weighted_sum
+    z_v_o = []
+    @out_nodes.times do |i|
+      z_v_o << scale_initial_weights[1].row(i).inner_product(vectorize_hidden_layer)
+    end
+    return z_v_o
+  end
+
+  def output_layer_activation
+    activation_function(output_layer_weighted_sum)
+  end
+
 end
--- spec/agnet_spec.rb
+++ spec/agnet_spec.rb
@@ -6,9 +6,18 @@ describe Agnet do
   end
 
   describe 'public instance methods' do
+    before do
+      @input_nodes = 4
+      @hidden_nodes = 16
+      @output_nodes = 10
+      @input_activation = [24, 155, 255, 0]
+      @input_bias = 1.0
+      @hidden_bias = 1.0
+      @bits = 255
+      @function = 'sigmax'
 
-    subject { Agnet.new(4, 16, 10) }
-
+    end
+    subject { Agnet.new(@input_nodes, @hidden_nodes, @output_nodes, @function, @input_activation, @input_bias, @hidden_bias, @bits) }
     let(:input) do
       [input_activation, weights_hidden, weights_output,
        input_nodes, hidden_nodes, output_nodes]
@@ -33,42 +42,142 @@ describe Agnet do
       end
       context '#set_initial_weights' do
         it 'sets initial hidden layer weight matrix' do
-          expect(subject.set_initial_weights[0].column_count).to eq(5)
-          expect(subject.set_initial_weights[0].row_count).to eq(16)
+          expect(subject.set_initial_weights[0].column_count)
+            .to eq(@input_nodes + 1)
+          expect(subject.set_initial_weights[0].row_count).to eq(@hidden_nodes)
         end
         it 'sets initial output layer weight matrix' do
-          expect(subject.set_initial_weights[1].column_count).to eq(17)
-          expect(subject.set_initial_weights[1].row_count).to eq(10)
+          expect(subject.set_initial_weights[1].column_count)
+            .to eq(@hidden_nodes + 1)
+          expect(subject.set_initial_weights[1].row_count).to eq(@output_nodes)
         end
         it 'sets initial hidden weight matrix values between 0.5 and -0.5' do
-          expect(subject.set_initial_weights[0][0,0]).to be_within(0.5).of(0)
+          expect(subject.set_initial_weights[0][0, 0]).to be_within(0.5).of(0)
         end
         it 'sets initial output weight matrix values between 0.5 and -0.5' do
-          expect(subject.set_initial_weights[1][0,0]).to be_within(0.5).of(0)
+          expect(subject.set_initial_weights[1][0, 0]).to be_within(0.5).of(0)
         end
       end
       context '#scale_initial_weights' do
         it 'mulitplys hidden initial factor to hidden weight matrix calcs' do
           init_weights = subject.set_initial_weights
           init_factors = subject.set_initial_factors
-          expect(subject.scale_initial_weights[0][0,0]).to eq(init_weights[0][0,0]*init_factors[0])
+
+          expect(subject.scale_initial_weights[0][0, 0])
+            .to eq(init_weights[0][0, 0] * init_factors[0])
         end
         it 'mulitplys output initial factor to ouput weight matrix calcs' do
           init_weights = subject.set_initial_weights
           init_factors = subject.set_initial_factors
-          expect(subject.scale_initial_weights[1][0,0]).to eq(init_weights[1][0,0]*init_factors[1])
+
+          expect(subject.scale_initial_weights[1][0, 0])
+            .to eq(init_weights[1][0, 0] * init_factors[1])
+        end
+      end
+      context '#normalize_input_activation' do
+        it 'returns a vector' do
+          expect(subject.normalize_input_activation).to be_a(Vector)
+        end
+        it 'returns vector with values between 0 and 1' do
+          expect(subject.normalize_input_activation[0])
+            .to be_within(0.5).of(0.5)
+        end
+        it 'has a vector size of input_nodes + 1 for a bias value' do
+          expect(subject.normalize_input_activation.size)
+            .to eq(@input_nodes + 1)
+        end
+      end
+      context '#hidden_layer_weighted_sum' do
+        it 'calcs weight sum vector for nodes in hdn layer with hdn weights' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.hidden_layer_weighted_sum[0])
+            .to eq(subject.scale_initial_weights[0]
+            .row(0).inner_product(@input_activation.map{ |r| r / 255}))
+        end
+        it 'returns array of size hidden nodes' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.hidden_layer_weighted_sum.size).to eq(@hidden_nodes)
+        end
+      end
+      context '#hidden_layer_activation' do
+        it 'applys activation function to each hidden node value' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.hidden_layer_activation)
+            .to eq(subject.activation_function(subject.hidden_layer_weighted_sum))
+        end
+      end
+      context '#activation_function' do
+        it 'returns select activation_function' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          #expect(subject.activation_function([.3,.6])).to eq(subject.hidden_layer_weighted_sum)
+        end
+      end
+
+      context '#vectorize_hidden_layer' do
+        it 'returns a vector' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.vectorize_hidden_layer).to be_a(Vector)
+        end
+        it 'the initial values do not change from array to vector ' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.vectorize_hidden_layer[0])
+            .to eq(subject.hidden_layer_activation[0])
+        end
+        it 'adds bias to end of vector' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.vectorize_hidden_layer.size)
+            .to eq(subject.hidden_layer_activation.size + 1)
+        end
+      end
+      context '#output_layer_weighted_sum' do
+        it 'calcs weight sum vector for nodes in hdn layer with hdn weights' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.output_layer_weighted_sum[0])
+            .to eq(subject.scale_initial_weights[1]
+            .row(0).inner_product(subject.vectorize_hidden_layer))
+        end
+        it 'returns array of size output nodes' do
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.output_layer_weighted_sum.size)
+            .to eq(@output_nodes)
+        end
+      end
+      context '#output_layer_activation' do
+        it 'applys activation function to each output node value' do
+
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          subject.set_initial_weights
+          subject.normalize_input_activation
+
+          expect(subject.output_layer_activation)
+            .to eq(subject.activation_function(subject.output_layer_weighted_sum))
         end
       end
       context '#feed_forward' do
-        it 'calcs weighted sum for each node in hdn layer with hdn weights' do
-        end
-
         it 'activates each node in hidden layer' do
         end
-
         it 'cals weighted sum for each node in outpt layer with otpt weights' do
         end
-
         it 'activates each node in output layer' do
         end
       end
