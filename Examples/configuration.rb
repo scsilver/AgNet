4b1b38f57c3e5042e7cbbcd031c854c5b81e6e55
Scsilver <scsilver.umd@gmail.com>
2016-02-10 03:42:30 -0700

    change configuration.rb example

--- Examples/configuration.rb
+++ Examples/configuration.rb
@@ -2,7 +2,7 @@ require 'agnet'
 
 net = Agnet.new(bits: 255, # dictated by data
                 input_nodes: 784, # dictated by data
-                hidden_nodes: 300, # the greater the number of nodes, the slower
+                hidden_nodes: 100, # the greater the number of nodes, the slower
                 output_nodes: 10, # dictated by classes, 10 for 10 digits 0-9
                 input_bias: 1.0, #hyperparameters
                 hidden_bias: 1.0,
@@ -11,10 +11,10 @@ net = Agnet.new(bits: 255, # dictated by data
                 learning_rate: 0.05
                 )
 
-net.train('Examples/train.csv', training_size: 1000, testing_size: 100)
+net.train('Examples/train.csv', training_size: 30, testing_size: 10)
 
 net.training_score
 
-net.test(test_first: 50) # only test first 50 in testing_data set
+net.test(test_first: 5) # only test first 5 in testing_data set
 
 net.testing_score
